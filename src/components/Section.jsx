f4981fe434526cad8f51771aefee269972978608
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2023-03-06 13:17:57 -0500

    form contact section added

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SectionTitle } from "./";
+import { SectionTitle, HobbiesCard } from "./";
 import { hobbies } from "../data/info";
 
 const Section = () => {
@@ -15,33 +15,7 @@ const Section = () => {
       </div>
       <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-8 mt-16">
         {hobbies.map((hobbie) => {
-          return (
-            <div
-              key={hobbies.title}
-              className="flex flex-col justify-between shadow-xl mt-4  dark:bg-modal px-8 py-7  h-full"
-            >
-              <header>
-                <div className="flex items-center gap-3">
-                  <div className="text-xl dark:text-white">{hobbie.icon}</div>
-                  <h3 className="text-xl font-bold dark:text-slate-200">
-                    {hobbie.title}
-                  </h3>
-                </div>
-                <div>
-                  <p className="text-slate-600 dark:text-slate-400 pt-3">
-                    {hobbie.description}
-                  </p>
-                </div>
-              </header>
-              <footer>
-                <div className="text-lg mt-5 dark:text-white">
-                  <a href={hobbie.website.link} className="hover:text-font inline-block">
-                    {hobbie.website.icon}
-                  </a>
-                </div>
-              </footer>
-            </div>
-          );
+          return <HobbiesCard hobbie={hobbie} />;
         })}
       </div>
     </section>
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,18 @@
-export { default as Burguer } from "./Burguer"
-export { default as ButtonTheme } from "./ButtonTheme"
-export { default as Hero } from "./Hero"
-export { default as Navbar } from "./Navbar"
-export { default as RightNav } from "./RightNav"
-export { default as SocialBar } from "./SocialBar"
-export { default as About } from "./About"
-export { default as Projects } from "./Projects"
-export { default as ProjectPreview } from "./ProjectPreview"
-export { default as SectionTitle } from "./SectionTitle"
-export { default as Section } from "./Section"
+export { default as Burguer } from "./Burguer";
+export { default as ButtonTheme } from "./ButtonTheme";
+export { default as Hero } from "./Hero";
+export { default as Navbar } from "./Navbar";
+export { default as RightNav } from "./RightNav";
+export { default as SocialBar } from "./SocialBar";
+export { default as About } from "./About";
+export { default as Projects } from "./Projects";
+export { default as ProjectPreview } from "./ProjectPreview";
+export { default as SectionTitle } from "./SectionTitle";
+export { default as Section } from "./Section";
+export { default as Contact } from "./Contact";
+export { default as HobbiesCard } from "./HobbiesCard";
+
+// Form
+export { default as Input } from "./form/Input";
+export { default as Label } from "./form/Label";
+export { default as ErrorMessage } from "./form/ErrorMessage";
